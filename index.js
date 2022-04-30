d7857801fd498c9b666a9db7afd2a975ec84d89d
Cody Murray <cdillon.murray@yahoo.com>
2022-04-30 14:53:27 -0500

    updated readme

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-require ('dotenv').config();
+require('dotenv').config();
 
 // Connect to database
 const db = mysql.createConnection(
     {
-      host: 'localhost',
-      // Your MySQL username,
-      user: process.env.DB_USER,
-      // Your MySQL password
-      password: process.env.DB_PASSWORD,
+        host: 'localhost',
+        // Your MySQL username,
+        user: process.env.DB_USER,
+        // Your MySQL password
+        password: process.env.DB_PASSWORD,
 
-      database: process.env.DB_NAME
+        database: process.env.DB_NAME
     },
     console.log(`Connected to the ${process.env.DB_NAME} database.`)
-  );
+);
+
+db.query(`SELECT * FROM department`, (err, rows) => {
+    console.log(rows);
+});
+
+// Delete a employee
+
+db.query(`DELETE FROM employee WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
+// Create a employee
+
+// ** example from module 12.2.4 below ** 
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+
+
+
 
